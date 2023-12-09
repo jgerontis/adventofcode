0984352ec482a0a62efe8ab263a8ff62efba9e44
Joshua Gerontis <joshua.gerontis@vasion.com>
2023-12-09 00:15:14 -0700

    day9 part2

--- 2023/day9/main.py
+++ 2023/day9/main.py
@@ -25,14 +25,32 @@ def get_value(row: list):
         level -= 1
     return triangle[0][-1]
 
+# tried reversing the input but it didn't work, so here I am doing it manually :(
+def get_value_p2(row: list):
+    triangle = [row]
+    level = 0
+    while True:
+        triangle.append([])
+        for i in range(len(triangle[level])-1):
+            diff = triangle[level][i+1] - triangle[level][i]
+            triangle[level+1].append(diff)
+        if diff == 0 or len(triangle[level+1]) == 0:
+            break
+        level += 1
+    level = len(triangle) - 1
+    triangle[-1].insert(0, 0)
+    while level > 0:   
+        triangle[level-1].insert(0, (triangle[level-1][0] - triangle[level][0]))
+        level -= 1
+    return triangle[0][0]
+
 def main():
     data = get_data()
     p1_total = 0
     p2_total = 0
     for row in data:
         p1_total += get_value(row.copy())
-        row.reverse()
-        p2_total += get_value(row)
+        p2_total += get_value_p2(row) # still mad reversing the input didn't work with my algorithm
     print("part1: ", p1_total)
     print("part2: ", p2_total)    
 
